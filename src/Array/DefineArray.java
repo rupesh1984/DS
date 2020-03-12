b43f0cb6ccc731636c372429e0399dcc710a7066
Rupesh Dubey <rupesh1984@gmail.com>
2020-03-12 13:32:19 +0530

    Basics of Array - 12-Mar-2020

---
+++ src/Array/DefineArray.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class DefineArray {
+
+
+
+public static void main(String args[]) {
+	int oneDArr[];
+	oneDArr = new int[3];
+	oneDArr[0] =10;
+	oneDArr[1] =20;
+	oneDArr[2] =30;
+	
+	
+	
+	System.out.print("Lenght of array is "+oneDArr.length);
+}
+	 
+}
---
+++ src/Core/Basic.java
@@ -0,0 +1,9 @@
+package Core;
+
+public class Basic {
+
+	public static void main (String args[]) {
+	
+		System.out.print("My name i s");
+	}
+}
